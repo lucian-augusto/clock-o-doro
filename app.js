f6d494588ca464b6760f1fba4862a1a6d95bc5e9
Lucian Augusto <lucian@Lucians-MacBook-Pro.local>
2019-11-18 10:26:41 -0300

    Fix listening port

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.post('/', function(req, res) {
 });
 
 // Setting up Listening Port
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Server running on port 3000.');
 });
